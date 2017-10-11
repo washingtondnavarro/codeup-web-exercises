bb1c31969d43b22314f23e2721d360f768cdcfdb
washingtondnavarro <washington.d.navarro@gmail.com>
2017-10-11 17:15:56 -0500

    Adding all classroom exercises up to Bootstrap intro exercise. These exercises are individually committed in the sequoia-web-exercises directory

---
+++ js/external.js
@@ -0,0 +1,9 @@
+"use strict";
+console.log("Hello from JB");
+
+alert("Welcome to my Website");
+
+var thebestcolor = prompt('What is your favorite color?');
+
+
+alert ('Great ' + thebestcolor+ ' is my favorite color too');
